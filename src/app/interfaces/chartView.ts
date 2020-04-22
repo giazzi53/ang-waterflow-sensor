0efc981ef16f1bcf23c62c30cef4977d526d2e4c
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2020-04-22 14:14:57 -0300

    comentando atributos desnecessários

--- src/app/interfaces/chartView.ts
+++ src/app/interfaces/chartView.ts
@@ -1,10 +1,10 @@
 import { DataPoint } from './dataPoint';
 
 export interface ChartView {
-  chartId: string;
-  username: string;
+  // chartId: string;
+  // username: string;
   title: string;
-  description: string;
+  // description: string;
   type: string;
   dataPoints: DataPoint[];
 }
