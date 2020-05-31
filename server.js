ebacc2744dfc37da3519cf332fb804dd60395353
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2020-05-31 12:43:34 -0300

    comentando cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //Install express server
 var path = require('path');
 var express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 var app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('dist/ang-waterflow-sensor'));
-app.use(cors());
+// app.use(cors());
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/ang-waterflow-sensor/index.html'));
